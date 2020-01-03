4a388e79bfd16bcb7d46e7ab2228f1e5da179266
Miriam Rauschert <miriam.rauschert@gmail.com>
2020-01-03 14:40:46 -0600

    Login / Registry Screen

---
+++ Assets/Scripts/GUI/gui_menu_textreveal.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+namespace TMPro.Examples
+{
+public class gui_menu_textreveal : MonoBehaviour
+{
+	public EventTrigger textEvent = null;
+    private TMP_Text m_TextComponent;
+    private bool hasTextChanged;
+
+    void Awake() {
+        m_TextComponent = gameObject.GetComponent<TMP_Text>();
+    }
+
+	void Start() {
+        StartCoroutine(RevealCharacters(m_TextComponent));
+    }
+
+    void OnEnable() {
+        // Subscribe to event fired when text object has been regenerated.
+        TMPro_EventManager.TEXT_CHANGED_EVENT.Add(ON_TEXT_CHANGED);
+    }
+
+    void OnDisable() {
+        TMPro_EventManager.TEXT_CHANGED_EVENT.Remove(ON_TEXT_CHANGED);
+    }
+
+
+    // Event received when the text object has changed.
+    void ON_TEXT_CHANGED(Object obj)
+    {
+        hasTextChanged = true;
+    }
+
+
+    /// <summary>
+    /// Method revealing the text one character at a time.
+    /// </summary>
+    /// <returns></returns>
+    IEnumerator RevealCharacters(TMP_Text textComponent) {
+
+        textComponent.ForceMeshUpdate();
+
+        TMP_TextInfo textInfo = textComponent.textInfo;
+
+        int totalVisibleCharacters = textInfo.characterCount; // Get # of Visible Character in text object
+        int visibleCount = 0;
+
+        while (true) {
+
+            if (hasTextChanged) {
+                totalVisibleCharacters = textInfo.characterCount; // Update visible character count.
+                hasTextChanged = false; 
+            }
+
+            if (visibleCount > totalVisibleCharacters) {
+            	if (textEvent != null) {
+            		textEvent.OnPointerDown(null);
+            	}
+                yield return null;
+            }
+
+            textComponent.maxVisibleCharacters = visibleCount; // How many characters should TextMeshPro display?
+
+            visibleCount += 1;
+
+            yield return new WaitForSeconds(0.05f);
+        }
+     }
+}
+}
---
+++ Assets/Scripts/UserHandling/mt_userhandling.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+
+public class mt_userhandling : MonoBehaviour {
+
+	public TMP_InputField r_nameField = null;
+	public TMP_InputField r_passField = null;
+	public TMP_InputField r_emailField = null;
+
+	public TMP_InputField l_nameField = null;
+	public TMP_InputField l_passField = null;
+
+	public void RegisterUser() {
+        StartCoroutine(RunRegister());
+	}
+    
+    IEnumerator RunRegister() {
+
+    	string url = "http://multiplayer.ninjachip.com/process/register.php";
+
+    	string sName = r_nameField.text;
+    	string sPassword = r_passField.text;
+    	string sEmail = r_emailField.text;
+
+	    List<IMultipartFormSection> formData = new List<IMultipartFormSection>();
+        formData.Add(new MultipartFormDataSection("username="+sName+"&password="+sPassword+"&email="+sEmail));
+
+        UnityWebRequest www = UnityWebRequest.Post(url, formData);
+        yield return www.SendWebRequest();
+
+        if (www.isNetworkError || www.isHttpError)
+        {
+            Debug.Log(www.error);
+        }
+        else
+        {
+            Debug.Log("Form upload complete!");
+        }
+    } 
+}
